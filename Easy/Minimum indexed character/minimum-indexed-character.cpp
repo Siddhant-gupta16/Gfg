4a96b32b46a33a500e220e4e46783bd0efb84477
siddhant gupta <95760026+Siddhant-gupta16@users.noreply.github.com>
2024-02-19 13:42:36 +0530

    Updated Solution - GfG to GitHub

--- Easy/Minimum indexed character/minimum-indexed-character.cpp
+++ Easy/Minimum indexed character/minimum-indexed-character.cpp
@@ -1,43 +1,68 @@
 //{ Driver Code Starts
+//Initial template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-class Solution{
-    public:
-	string printMinIndexChar(string S, string patt)
-	{   int n = S.size();
-	     string ans ="";
-	   
-	   for(int i=0;i<n;i++){
-	       if(patt.find(S[i])!=string::npos){
-	           ans = S[i];
-	           break;
-	       }
-	   }
-	   if(ans.empty()){
-	       return "$";
-	   }
-	   return ans ;
-	}
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to find the minimum indexed character.
+    int minIndexChar(string str, string patt)
+    {
+    //   int n = patt.length();
+    //   int ans =-1;
+       
+    //   for(int i=0;i<n;i++){
+    //       string s ="";
+    //       s= patt[i];
+           
+    //       if(s.find(str)!=std::string::npos){
+    //           int a = s.find(str);
+    //           cout<<a<<endl;
+    //           if(a>ans){
+    //               ans =a;
+    //           }
+    //       }
+           
+    //       cout<<s<<endl;
+           
+    //   }
+       
+    //   return ans;
+    unordered_set<char> pattSet(patt.begin(), patt.end()); 
+
+    for (int i = 0; i < str.length(); ++i) {
+        if (pattSet.find(str[i]) != pattSet.end()) {
+            return i; 
+        }
+    }
+    
+    return -1; 
+}
+    
 };
 
 //{ Driver Code Starts.
-int main() {
-    int t;
+
+int main()
+{
+	int t;
     cin>>t;
-    
     while(t--)
     {
         string str;
-        cin>>str;
         string patt;
-        cin >> patt;
+        cin>>str;
+        cin>>patt;
         Solution obj;
-        cout<<obj.printMinIndexChar(str, patt)<<endl;
+        cout<<obj.minIndexChar(str, patt)<<endl;
     }
-    
 	return 0;
 }
+
 // } Driver Code Ends
