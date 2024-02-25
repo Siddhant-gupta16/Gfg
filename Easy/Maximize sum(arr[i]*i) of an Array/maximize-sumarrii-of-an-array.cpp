a7fef5d01c8d119b7219ec87589614be921f5768
siddhant gupta <95760026+Siddhant-gupta16@users.noreply.github.com>
2024-02-25 13:37:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximize sum(arr[i]*i) of an Array/maximize-sumarrii-of-an-array.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int Maximize(int a[],int n)
+    {
+       sort(a,a+n);
+       long long int  ans =0;
+        const long long int MOD = 1000000007; 
+       
+       for(int i=0;i<n;i++){
+           ans = (ans + (static_cast<long long int>(a[i]) * i)) % MOD;
+       }
+    //   ans = ans % (long long int )(pow(10,9) + 7);
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<<ob.Maximize(a,n)<<endl;
+    }
+}
+// } Driver Code Ends
